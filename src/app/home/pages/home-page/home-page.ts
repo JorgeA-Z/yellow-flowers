437e7292dd257cb13a20120a1d27242b51161da5
System Administrator <root@MacBook-Air-de-J.local>
2026-09-22 12:33:43 -0600

    chore: angular updated & pnpm installed

--- src/app/home/pages/home-page/home-page.ts
+++ src/app/home/pages/home-page/home-page.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   imports: [],
+  changeDetection: ChangeDetectionStrategy.Eager,
   templateUrl: './home-page.html'
 })
 export class HomePage {
--- src/app/utils/pages/not-found/not-found.ts
+++ src/app/utils/pages/not-found/not-found.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   imports: [],
+  changeDetection: ChangeDetectionStrategy.Eager,
   templateUrl: './not-found.html'
 })
 export class NotFound {
